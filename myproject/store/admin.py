a31e066d30435532e2645fcc4c1cceffe5d5fb55
peikra <mikheil.peikrishvili.1@btu.edu.ge>
2024-10-17 19:31:02 +0400

    update

--- myproject/store/admin.py
+++ myproject/store/admin.py
@@ -4,17 +4,16 @@ from .models import Product, Category
 
 # Register your models here.
 
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent')
     search_fields = ('name',)
     list_filter = ('parent',)
-
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
     search_fields = ('name',)
     filter_horizontal = ('categories',)
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
+
 
