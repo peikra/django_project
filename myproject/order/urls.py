2110cca9ef96892b7a1bd013fc56a3303f144aee
peikra <mikheil.peikrishvili.1@btu.edu.ge>
2024-10-28 22:39:07 +0400

    update

--- myproject/order/urls.py
+++ myproject/order/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('order/cart', views.CartView.as_view(), name='cart'),
-    path('order/checkout/', views.CheckoutView.as_view(), name='checkout'),
-    path('order/cartitems', views.CartView.as_view(), name='cartitem'),
+    path('cart', views.CartView.as_view(), name='cart'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+    path('cartitems', views.CartView.as_view(), name='cartitem'),
     path('cartitems/<int:product_id>/', views.DeleteProductView.as_view(), name='delete_product')
 
 
